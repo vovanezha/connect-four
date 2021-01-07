422001644f353d7cd33df94edd0ced2653f3b0aa
vovanezha <vova19tup@gmail.com>
2021-01-07 16:40:01 +0300

    configure parcel and react

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <h1>Hello, Connect Two!</h1>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
